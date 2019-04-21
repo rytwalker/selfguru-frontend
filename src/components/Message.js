77dcfdef29560d8bb3bd537e636af407408308b1
Ryan Walker <rytwalker@gmail.com>
2019-04-21 17:27:37 -0400

    got a date picker working

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,17 +1,27 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 const Message = ({ title, newMessage }) => {
   const [message, setMessage] = useState(title);
+  const [startDate, setStartDate] = useState(new Date());
   const [editMessage, setEditMessage] = useState(false);
+
+  function submitMessage(e) {
+    e.preventDefault();
+    console.log(message);
+    console.log(startDate);
+  }
+
   return (
-    <StyledMessage>
+    <StyledMessage onSubmit={submitMessage}>
       <MessageTab onClick={() => setEditMessage(!editMessage)}>
         {message}
       </MessageTab>
       {editMessage && (
-        <form>
-          <div className="form-group">
+        <MessageForm>
+          <FromGroup>
             <label htmlFor="message">Message:</label>
             <input
               type="text"
@@ -20,18 +30,23 @@ const Message = ({ title, newMessage }) => {
               name="message"
               id="message"
             />
-          </div>
-          <div className="form-group">
+          </FromGroup>
+          <FromGroup>
             <label htmlFor="message">To:</label>
             <p>rytwalker</p>
-          </div>
-          <div className="form-group">
+          </FromGroup>
+          <FromGroup>
             <label htmlFor="message">Send it:</label>
-            <p>Today April 21st, 2019 at 8:00pm</p>
-          </div>
-          <button type="submit">Update</button>
+            <DatePicker
+              selected={startDate}
+              onChange={date => setStartDate(date)}
+              showTimeSelect
+              dateFormat="Pp"
+            />
+          </FromGroup>
+          <button type="submit">{newMessage ? 'Add' : 'Update'}</button>
           <button>Delete</button>
-        </form>
+        </MessageForm>
       )}
     </StyledMessage>
   );
@@ -40,11 +55,33 @@ const Message = ({ title, newMessage }) => {
 const StyledMessage = styled.div`
   display: flex;
   flex-direction: column;
+  border-bottom: 1px solid black;
 `;
 
 const MessageTab = styled.div`
-  padding: 2rem 0;
+  padding: 2rem;
   cursor: pointer;
+  border-bottom: 1px solid #fefefe;
+`;
+
+const MessageForm = styled.form`
+  width: 100%;
+  background: #efefef;
+  padding: 2rem;
+`;
+
+const FromGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 2rem;
+  label {
+    font-size: 1.2rem;
+    margin-bottom: 0.5rem;
+  }
+  input {
+    font-size: inherit;
+    padding: 1rem 1.5rem;
+  }
 `;
 
 export default Message;
